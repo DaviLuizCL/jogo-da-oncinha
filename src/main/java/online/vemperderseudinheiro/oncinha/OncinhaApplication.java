2b023f832896c43c5c2c572f5a53ad5ef48a01c2
devfelipevitorino <devfelipevitorino@gmail.com>
2024-10-07 18:40:28 -0300

    starting project scope

---
+++ src/main/java/online/vemperderseudinheiro/oncinha/OncinhaApplication.java
@@ -0,0 +1,12 @@
+package online.vemperderseudinheiro.oncinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OncinhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OncinhaApplication.class, args);
+	}
+}
